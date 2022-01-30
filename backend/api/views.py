d5ea760740b8e6c8b539f543a44bd5307a6d27a8
hiseoung <anhisg4104@gmail.com>
2022-01-30 14:19:13 +0900

    Test PR template

--- backend/api/views.py
+++ backend/api/views.py
@@ -14,47 +14,6 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer()
 
-
-# class DesignViewSet(mixins.RetrieveModelMixin,
-#                     mixins.UpdateModelMixin,
-#                     mixins.DestroyModelMixin,
-#                     generics.GenericAPIView):
-#     queryset = goods_design.objects.all()
-#     serializer_class = DesignSerializer()
-#     permission_classes = [IsAuthenticated]
-
-#     def get(self, request, *args, **kwargs):
-#         design = goods_design.objects.all()
-#         return Response(design)
-
-#     def put(self, request, *args, **kwargs):
-#         design = self.get_object()
-#         return self.response
-
-#     def delete(self, request, *args, **kwargs):
-#         return self.destroy(request, *args, **kwargs)
-
-#     def create(self, request, *args, **kwargs):
-#         return super().create(request, *args, **kwargs)
-
-#     def list(self, request, *args, **kwargs):
-#         return super().list(request, *args, **kwargs)
-
-#     def retrieve(self, request, *args, **kwargs):
-#         return super().retrieve(request, *args, **kwargs)
-
-#     def get_serializer_class(self):
-#         if self.action == 'retrieve':
-#             return DesignSerializer
-#         return super().get_serializer_class()
-
-#     def get_queryset(self):
-#         qs = super().get_queryset()
-#         return qs.all()
-
-#     def perform_create(self, serializer):
-#         serializer.save(owner=self.request.user)
-
 class DesignViewSet(viewsets.ModelViewSet):
 
     queryset = goods_design.objects.all()
