766d9c301b6dca144001a7e390bb32076907a6d3
hiseoung <anhisg4104@gmail.com>
2022-02-01 16:34:22 +0900

    Change models.py in api and related viewsets

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import goods_design
+from .models import goods_design, goods_info, goods_result
 from django.contrib.auth.models import User
 
         
@@ -9,7 +9,20 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'password']
 
 
+class UploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = goods_info
+        fields = '__all__'
+
+
 class DesignSerializer(serializers.ModelSerializer):
     class Meta:
         model = goods_design
         fields = '__all__'
+
+
+class ResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = goods_result
+        fields = '__all__'
+
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
-from .views import UserViewSet, DesignViewSet
+from .views import UserViewSet, DesignViewSet, UploadViewSet, ResultViewSet
 from rest_framework.routers import DefaultRouter
 from django.conf.urls.static import static
 from django.conf import settings
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
+router.register('upload', UploadViewSet, basename='upload')
 router.register('designs', DesignViewSet, basename='designs')
+router.register('results', ResultViewSet, basename='results')
 
 app_name = 'api'
 
