21b612a3159c529ac516ba837c6892aec81be0c6
hiseoung <anhisg4104@gmail.com>
2022-02-09 23:33:55 +0900

    Update csrf_exempt in login

--- backend/users/views.py
+++ backend/users/views.py
@@ -5,9 +5,12 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from .models import profile
 
 
+@method_decorator(csrf_exempt, name = "dispatch")
 class RegisterView(APIView):
     '''
     - 사용자가 데이터를 입력해 회원가입을 진행합니다.
@@ -34,6 +37,8 @@ class RegisterView(APIView):
         token = Token.objects.create(user=user)
         return Response({"Token": token.key})
 
+
+@method_decorator(csrf_exempt, name = "dispatch")
 class LoginView(APIView):
     '''
     - 사용자가 입력한 데이터를 받아서 로그인을 합니다.
