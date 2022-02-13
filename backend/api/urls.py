5d71c24e65540b344737cc11b6362a4bf25ef303
hiseoung <anhisg4104@gmail.com>
2022-02-13 15:24:38 +0900

    Update media root

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from .views import UserViewSet, DesignViewSet, UploadViewSet, ResultViewSet
 from rest_framework.routers import DefaultRouter
+from .import views
 from django.conf.urls.static import static
 from django.conf import settings
-from .import views
 
 router = DefaultRouter()
 router.register('users', UserViewSet)
@@ -15,5 +15,5 @@ app_name = 'api'
 
 urlpatterns = [
     path('api/', include(router.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
+]
 
