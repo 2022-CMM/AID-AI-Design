2faec09ce311cf406542d90c9e85829e83182c69
hiseoung <anhisg4104@gmail.com>
2022-02-13 14:25:07 +0900

    Update models.py in api

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,12 +10,12 @@ from .models import goods_design, goods_result, goods_info
 from .serializers import UserSerializer, DesignSerializer, UploadSerializer, ResultSerializer
 import os, re
 from typing import List
-import click
+# import click
 # from ...ai.stylegan2_ada_pytorch import dnnlib
 # from ...ai.stylegan2_ada_pytorch import legacy
-import numpy as np
-import PIL.Image
-import torch
+# import numpy as np
+# import PIL.Image
+# import torch
 
 
 class UserViewSet(viewsets.ModelViewSet):
