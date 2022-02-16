80cbca893bea054e26c05f8de329bbbf3520e77e
jongmin4422 <jongmin4422@naver.com>
2022-02-16 22:28:19 +0900

    add a style mix code made by @LimSeJin9577

--- ai/stylegan2_ada_pytorch/generate.py
+++ ai/stylegan2_ada_pytorch/generate.py
@@ -34,23 +34,25 @@ def num_range(s: str) -> List[int]:
 
 #----------------------------------------------------------------------------
 
-@click.command()
-@click.pass_context
-@click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
-@click.option('--seeds', type=num_range, help='List of random seeds')
-@click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
-@click.option('--class', 'class_idx', type=int, help='Class label (unconditional if not specified)')
-@click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
-@click.option('--projected-w', help='Projection result file', type=str, metavar='FILE')
-@click.option('--outdir', help='Where to save the output images', type=str, required=True, metavar='DIR')
+# @click.command()
+# @click.pass_context
+# @click.option('--network', 'network_pkl', help='Network pickle filename', required=True)
+# @click.option('--seeds', type=num_range, help='List of random seeds')
+# @click.option('--trunc', 'truncation_psi', type=float, help='Truncation psi', default=1, show_default=True)
+# @click.option('--class', 'class_idx', type=int, help='Class label (unconditional if not specified)')
+# @click.option('--noise-mode', help='Noise mode', type=click.Choice(['const', 'random', 'none']), default='const', show_default=True)
+# @click.option('--projected-w', help='Projection result file', type=str, metavar='FILE')
+# @click.option('--outdir', help='Where to save the output images', type=str, required=True, metavar='DIR')
 def generate_images(
-    ctx: click.Context,
-    network_pkl: str,
+    # ctx: click.Context,
+    # network_pkl: str,
+    device,
+    G,
     seeds: Optional[List[int]],
     truncation_psi: float,
     noise_mode: str,
     outdir: str,
-    class_idx: Optional[int],
+    # class_idx: Optional[int],
     projected_w: Optional[str]
 ):
     """Generate images using pretrained network pickle.
@@ -78,10 +80,10 @@ def generate_images(
         --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
     """
 
-    print('Loading networks from "%s"...' % network_pkl)
-    device = torch.device('cuda')
-    with dnnlib.util.open_url(network_pkl) as f:
-        G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
+    # print('Loading networks from "%s"...' % network_pkl)
+    # device = torch.device('cuda')
+    # with dnnlib.util.open_url(network_pkl) as f:
+    #     G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
 
     os.makedirs(outdir, exist_ok=True)
 
@@ -99,15 +101,16 @@ def generate_images(
             img = PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/proj{idx:02d}.png')
         return
 
-    if seeds is None:
-        ctx.fail('--seeds option is required when not using --projected-w')
+    # if seeds is None:
+    #     ctx.fail('--seeds option is required when not using --projected-w')
 
     # Labels.
     label = torch.zeros([1, G.c_dim], device=device)
     if G.c_dim != 0:
-        if class_idx is None:
-            ctx.fail('Must specify class label with --class when using a conditional network')
-        label[:, class_idx] = 1
+        # if class_idx is None:
+        #     ctx.fail('Must specify class label with --class when using a conditional network')
+        # label[:, class_idx] = 1
+        pass
     else:
         if class_idx is not None:
             print ('warn: --class=lbl ignored when running on an unconditional network')
---
+++ ai/stylegan2_ada_pytorch/run.py
@@ -0,0 +1,51 @@
+import numpy as np
+import dnnlib
+import torch
+import legacy
+
+from style_mixing import generate_style_mix
+from projector import run_projection
+
+if __name__ == "__main__" :
+    seed = 2021
+    network_pkl = '../model/network-snapshot-000080.pkl'
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    # Load networks.
+    print('Loading networks from "%s"...' % network_pkl)
+    device = torch.device('cuda')
+
+    with dnnlib.util.open_url(network_pkl) as fp:
+        G = legacy.load_network_pkl(fp)['G_ema'].requires_grad_(False).to(device) # type: ignore
+
+    '''
+    Image1_url = "./test_images/test1.png"
+    Image2_url = "./test_images/test2.png"
+    '''
+
+    # Image1, 2에 대한 latent vector를 생성
+    w_dict = {}
+    for i in range(1,3) :
+        print(f"이미지를 생성합니다. ===> Input : {i}")
+        projected_w = run_projection(G = G,
+            target_fname = f'./test_images/test{i}.png',
+            outdir = 'output',
+            save_video = True,
+            seed = seed,
+            num_steps = 1000,
+            device = device
+        )
+        w_dict[f"Image{i}"] = projected_w
+    # print(w_dict["Image1"][0:6])
+
+    # Style Mixing
+    print("Style Mixing...")
+    generate_style_mix(
+        G = G,
+        w_dict = w_dict,
+        col_start = 0,
+        col_end = 6,
+        noise_mode='const',
+        outdir='output'
+    )
