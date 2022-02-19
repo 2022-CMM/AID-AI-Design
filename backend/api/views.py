866097d4bd1a5e8d02a789039bed3154fd4112df
hiseoung <anhisg4104@gmail.com>
2022-02-19 22:51:51 +0900

    Update upload view

--- backend/api/views.py
+++ backend/api/views.py
@@ -51,13 +51,11 @@ class UploadViewSet(viewsets.ModelViewSet):
         serializers = UploadSerializer(queryset, many=True)
         return Response(serializers.data)
 
-    def create(self, request, *args, **kwargs):
+    def create(self, request):
         print(request.body)
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        queryset = goods_info.objects.all()
+        serializers = UploadSerializer(queryset, many=True)
+        return Response(serializers.data)
 
 
 class DesignViewSet(viewsets.ModelViewSet):
